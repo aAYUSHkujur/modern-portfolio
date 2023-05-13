dd4ba040487696000d7145940bf541a556181f4b
aayushk720@gmail.com <aayushk720@gmail.com>
2023-05-13 23:02:36 +0530

    updates

--- utils/fetchPageInfo.ts
+++ utils/fetchPageInfo.ts
@@ -1,13 +1,15 @@
 import { PageInfo } from "../typings";
 
 export const fetchPageInfo = async () => {
-  // const res = await fetch(
-  //   `http://localhost:3000//api/getPageInfo`
-  // );
+  const res = await fetch(
+    `http://localhost:3000//api/getPageInfo`
+  );
 
-  // const data = await res.json();
+  
 
-  // const pageInfo: PageInfo = data.pageInfo;
+  const data = await res.json();
 
-  // return pageInfo;
+  const pageInfo: PageInfo = data.pageInfo;
+
+  return pageInfo;
 };
