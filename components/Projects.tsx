311088389754aacc909df2ed6e9bcb8d0b7a27c3
aayushk720@gmail.com <aayushk720@gmail.com>
2023-05-13 19:15:48 +0530

    updates

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Project } from "../typings";
 import { urlFor } from "../sanity";
 import Image from "next/image";
+import Link from "next/link";
 
 type Props = {
   projects: Project[];
@@ -23,15 +24,28 @@ function Projects({ projects }: Props) {
             key={project._id}
             className="p-5 mt-5 w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center sm:p-20 sm:mt-0 md:p-44 sm:h-screen"
           >
-            <motion.img
-              initial={{ y: -300, opacity: 0 }}
-              transition={{ duration: 1.2 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              viewport={{ once: true }}
-              className="sm:h-[35vh] mt-[12vh] 2xl:h-[40vh] 2xl:mt-[6vh] sm:object-contain cursor-pointer"
-              src={urlFor(project?.image).url()}
-              alt=""
-            />
+            <Link href={project?.linkToBuild} passHref>
+              <a target="_blank">
+                <div
+                  className="flex relative transition-all cursor-pointer hover:after:content-['Link_to_Build'] after:opacity-0 hover:after:opacity-100 after:absolute after:text-xl after:scale-50 hover:after:scale-100  after:top-1/2 after:left-1/2 after:-translate-x-1/2 after:-translate-y-1/2 after:bg-gray-900/70 after:shadow-black/80 after:shadow-xl after:text-[#FFE55C]/90 after:px-4 after:py-2 after:mt-[6vh]
+                             2xl:after:mt-[3vh] after:rounded-full md:after:text-2xl hover:after:hover:underline after:transition-all"
+                >
+                  <motion.img
+                    initial={{ y: -300, opacity: 0 }}
+                    transition={{ duration: 1.2 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    viewport={{ once: true }}
+                    className="sm:h-[35vh] mt-[12vh] 2xl:h-[40vh] 2xl:mt-[6vh] sm:object-contain cursor-pointer"
+                    src={urlFor(project?.image).url()}
+                    alt=""
+                  />
+                  <p className="opacity-0 w-fit h-fit hover:opacity-100 duration-300 absolute text-xl flex justify-center inset-0 text-white font-semibold shadow-2xl sm:object-contain cursor-pointer items-center rounded-lg underline">
+                    Live Showcase
+                  </p>
+                </div>
+              </a>
+            </Link>
+
             <div
               className="space-y-3
             sm:space-y-5 2xl:space-y-10 px-0 md:px-10 max-w-6xl"
