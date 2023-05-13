65704a94077e571bfbdf2f4e156ed4f7d5064824
aayushk720@gmail.com <aayushk720@gmail.com>
2023-05-13 22:52:26 +0530

    updates

--- utils/fetchPageInfo.ts
+++ utils/fetchPageInfo.ts
@@ -2,11 +2,11 @@ import { PageInfo } from "../typings";
 
 export const fetchPageInfo = async () => {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/getPageInfo`
+    `http://localhost:3000//api/getPageInfo`
   );
 
   const data = await res.json();
-  
+
   const pageInfo: PageInfo = data.pageInfo;
 
   return pageInfo;
