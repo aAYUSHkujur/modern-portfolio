eab39baae770b2091482c9ef1b449ce7025cd6af
aayushk720@gmail.com <aayushk720@gmail.com>
2023-05-13 23:08:47 +0530

    updates

--- utils/fetchPageInfo.ts
+++ utils/fetchPageInfo.ts
@@ -2,11 +2,11 @@ import { PageInfo } from "../typings";
 
 export const fetchPageInfo = async () => {
   const res = await fetch(
-    `https://modern-portfolio-aayushkujur.vercel.app/api/getPageInfo`
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/getPageInfo`
   );
 
   const data = await res.json();
-
+  
   const pageInfo: PageInfo = data.pageInfo;
 
   return pageInfo;
